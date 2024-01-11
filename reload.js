90cf1db52734228ef3e5f0119c5fc6b8856b1ff7
tyouse1 <tyouse1@live.com>
2024-01-11 14:44:44 -0500

    added a input box for the user so that they can choose how many images they would like to load

--- reload.js
+++ reload.js
@@ -1,51 +1,46 @@
-
-export async function fetchAndDisplayImages() {
-  const url = `https://api.thedogapi.com/v1/images/search?limit=21`;
-  const api_key = "live_M5iSn1Ms9iVph8qjvE9WPWl9A1N6FX5NzmmFXoE8qFpSycE7BRo2XSwtUHV10VjT";
-  fetch(url, {
-    headers: {
-      "x-api-key": api_key,
-    },
-  })
-    .then((response) => {
+// imageModule.js
+export async function fetchAndDisplayImages(imageLimit) {
+    const url = `https://api.thedogapi.com/v1/images/search?limit=${imageLimit || 15}`;
+    const api_key = "live_M5iSn1Ms9iVph8qjvE9WPWl9A1N6FX5NzmmFXoE8qFpSycE7BRo2XSwtUHV10VjT";
+  
+    try {
+      const response = await fetch(url, {
+        headers: {
+          "x-api-key": api_key,
+        },
+      });
+  
       if (!response.ok) {
         throw new Error(`HTTP error! Status: ${response.status}`);
       }
-      return response.json();
-    })
-    .then((data) => {
+  
+      const data = await response.json();
+  
       clearGrid(); // Clear existing images before adding new ones
-
+  
       let imagesData = data;
       imagesData.map(function (imageData) {
         let image = document.createElement("img");
-        // use the url from the image object
+        // url from the image object
         image.src = `${imageData.url}`;
-
+  
         let gridCell = document.createElement("div");
         gridCell.classList.add("col");
         gridCell.classList.add("col-lg");
         gridCell.appendChild(image);
-
+  
         document.getElementById("grid").appendChild(gridCell);
       });
-    })
-    .catch(function (error) {
-      console.log(error);
-    });
-}
-
-export function clearGrid() {
-  // Clear existing images in the grid
-  const grid = document.getElementById("grid");
-  while (grid.firstChild) {
-    grid.removeChild(grid.firstChild);
+    } catch (error) {
+      console.error("Oh no, something went wrong!", error);
+    }
   }
-}
-
-// Add a button click event listener to trigger image loading
-const loadImagesButton = document.getElementById("loadImagesButton");
-loadImagesButton.addEventListener("click", fetchAndDisplayImages);
-
-// Initial load of images
-fetchAndDisplayImages();
+  
+  export function clearGrid() {
+    // Clear existing images in the grid
+    const grid = document.getElementById("grid");
+    while (grid.firstChild) {
+      grid.removeChild(grid.firstChild);
+    }
+  }
+  
